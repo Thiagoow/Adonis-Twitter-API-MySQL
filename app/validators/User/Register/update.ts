cecea9d2f4b71c473cca10842a1a3b58429aac1f
Thiago Lopes <thiagoslopes8@outlook.com>
2024-04-07 19:02:41 -0300

    Repaired User/Register/update validator

--- app/validators/User/Register/update.ts
+++ app/validators/User/Register/update.ts
@@ -8,12 +8,6 @@ export const UpdateValidator = vine.compile(
       .exists(async (db, value) => {
         return !!(await db.from('user_keys').where('key', value))
       }),
-    username: vine
-      .string()
-      .trim()
-      .unique(async (db, value) => {
-        return !(await db.from('users').where('username', value).first())
-      }),
     fullName: vine.string().trim(),
     password: vine.string().trim().confirmed({ confirmationField: 'passwordConfirmation' }),
   })
