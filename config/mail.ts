3e3ebb7ada106072607b8145992b62edad7e3c36
Thiago Lopes <thiagoslopes8@outlook.com>
2024-05-10 13:45:22 -0300

    Repaired mail config

--- config/mail.ts
+++ config/mail.ts
@@ -13,10 +13,7 @@ const mailConfig = defineConfig({
     smtp: transports.smtp({
       host: env.get('SMTP_HOST'),
       port: env.get('SMTP_PORT'),
-      /**
-       * Uncomment the auth block if your SMTP
-       * server needs authentication
-       */
+      secure: false,
       auth: {
         type: 'login',
         user: env.get('SMTP_USERNAME') || '',
